919b0870e1721889ae45ebd3251ceaf3db7e11a3
gabe <gabe.driggs@gmail.com>
2025-12-08 19:52:01 -0700

    feat: center the command input

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -23,6 +23,24 @@ return {
     event = 'VeryLazy',
     dependencies = { 'MunifTanjim/nui.nvim', 'rcarriga/nvim-notify' },
     opts = {
+      cmdline = {
+        enabled = true,
+        view = 'cmdline_popup', -- Centered popup for command line
+        opts = {},
+        format = {
+          cmdline = { pattern = '^:', icon = '', lang = 'vim' },
+          search_down = { kind = 'search', pattern = '^/', icon = ' ', lang = 'regex' },
+          search_up = { kind = 'search', pattern = '^%?', icon = ' ', lang = 'regex' },
+          filter = { pattern = '^:%s*!', icon = '$', lang = 'bash' },
+          lua = { pattern = { '^:%s*lua%s+', '^:%s*lua%s*=%s*', '^:%s*=%s*' }, icon = '', lang = 'lua' },
+          help = { pattern = '^:%s*he?l?p?%s+', icon = '󰋖' },
+          input = {},
+        },
+      },
+      popupmenu = {
+        enabled = true,
+        backend = 'nui', -- Use nui for better styling
+      },
       lsp = {
         override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
@@ -34,10 +52,44 @@ return {
         { filter = { event = 'msg_show', any = { { find = '%d+L, %d+B' }, { find = '; after #%d+' }, { find = '; before #%d+' } } }, view = 'mini' },
       },
       presets = {
-        bottom_search = true,
-        command_palette = true,
+        bottom_search = false, -- Use popup for search too
+        command_palette = true, -- VSCode-style command palette
         long_message_to_split = true,
         lsp_doc_border = true,
+      },
+      views = {
+        cmdline_popup = {
+          position = {
+            row = '40%', -- 40% from top = roughly center
+            col = '50%',
+          },
+          size = {
+            width = 60,
+            height = 'auto',
+          },
+          border = {
+            style = 'rounded',
+            padding = { 0, 1 },
+          },
+          win_options = {
+            winhighlight = 'Normal:Normal,FloatBorder:FloatBorder',
+          },
+        },
+        popupmenu = {
+          relative = 'editor',
+          position = {
+            row = '50%',
+            col = '50%',
+          },
+          size = {
+            width = 60,
+            height = 10,
+          },
+          border = {
+            style = 'rounded',
+            padding = { 0, 1 },
+          },
+        },
       },
     },
   },
